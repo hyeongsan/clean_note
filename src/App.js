88828fda92160b1c5c0d1e482fc73d37b873f90a
san <sanstory12rt@naver.com>
2021-12-31 15:51:35 +0900

    tesing

--- src/App.js
+++ src/App.js
@@ -2,25 +2,29 @@ import React, { useState, useEffect } from "react";
 // import Navbar from "./components/Navbar";
 import routes from "./routes";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/home";
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
-      <div className="note">
-        <Switch>
-          {routes.map((route) => {
-            return (
-              <Route
-                exact
-                key={route.path}
-                path={route.path}
-                component={route.component}
-              />
-            );
-          })}
-        </Switch>
-      </div>
-    </Router>
+    // <Router basename={process.env.PUBLIC_URL}>
+    //   <div className="note">
+    //     <Switch>
+    //       {routes.map((route) => {
+    //         return (
+    //           <Route
+    //             exact
+    //             key={route.path}
+    //             path={route.path}
+    //             component={route.component}
+    //           />
+    //         );
+    //       })}
+    //     </Switch>
+    //   </div>
+    // </Router>
+    <div className="note">
+      <Home />
+    </div>
   );
 }
 
